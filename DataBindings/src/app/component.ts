0a28a5af77fef0733ec2bd09f834a6c0c7a5818e
EduardoEbbers <cel.ebbers@gmail.com>
2021-03-24 16:04:20 -0300

    Property/Interpolation/Attribute/Class/Style Bindings

--- DataBindings/src/app/component.ts
+++ DataBindings/src/app/component.ts
@@ -10,4 +10,34 @@ import { Component } from '@angular/core';
 export class ProductComponent {
     // cria o objeto Model que é o repositorio para poder acessar variaveis e/ou metodos
     model = new Model();
+
+    fontSizeWithUnits: string = "30px";
+    fontSizeWithoutUnits: string = "30";
+
+    // retorna um string do qual se a quantidade dos produtos for true vai setar "bg-success", se for false vai setar "bg-warning"
+    getClasse(): string  {
+        return this.model.getProducts().length == 5 ? "bg-success" : "bg-warning";
+    }
+
+    getClasses(key: number): string {
+        let product = this.model.getProduct(key);
+        return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
+    }
+
+    getClassesMap(key: number): Object {
+        let product = this.model.getProduct(key);
+        return {
+            "text-center bg-danger": product.name == "Kayak",
+            "bg-info": product.price < 50
+        };
+    }
+
+    getStylesMap(key: number) {
+        let product = this.model.getProduct(key);
+        return {
+            fontSize: "30px",
+            "margin.px": 100,
+            color: product.price < 50 ? "red" : "green"
+        };
+    }
 }
