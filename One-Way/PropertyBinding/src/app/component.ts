5a538ce93fac71110580bda731182130855bda00
EduardoEbbers <cel.ebbers@gmail.com>
2021-03-26 16:26:03 -0300

    standard property/interpolation/attribute/class/style binding

--- One-Way/PropertyBinding/src/app/component.ts
+++ One-Way/PropertyBinding/src/app/component.ts
@@ -7,4 +7,16 @@ import { Component } from "@angular/core";
 })
 export class ProductComponent {
     model: Model = new Model();
+
+    fontSizeComUnidades: string = "30px";
+    fontSizeSemUnidades: string = "30";
+
+    getClass(): string {
+        return this.model.getProducts().length == 5 ? "bg-info" : "bg-warning";
+    }
+
+    getClasses(key: number): string {
+        let product = this.model.getProduct(key);
+        return "p-2 " + (product.price < 50 ? "bg-danger" : "bg-warning");
+    }
 }
